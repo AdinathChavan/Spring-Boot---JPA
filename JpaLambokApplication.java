f0c655dbee2bbf885215ec6bb85371b09e5c38b9
AdinathChavan <adinath4356@gmail.com>
2025-07-18 08:18:55 +0530

    spring.hibernate.sql.ddl.auto  property (danger)
    No real time use

--- JpaLambokApplication.java
+++ JpaLambokApplication.java
@@ -16,12 +16,16 @@ public class JpaLambokApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext container = SpringApplication.run(JpaLambokApplication.class, args);
 //		
-//		Office data = container.getBean(Office.class);
+		Office data = container.getBean(Office.class);
 //		//insert operation in database
-//		data.addData();
-//		System.out.println("-------printing data from database-------");
-//		//printing/ select operation in database
-//		data.printData();
+		data.addData();
+		System.out.println("-------printing data from database-------");
+		//printing/ select operation in database
+		data.printData();
+		
+		UpdateData update = container.getBean(UpdateData.class);
+		
+		
 //		
 //		//getting primary data @Id
 //		data.getSpecificData();
@@ -30,15 +34,17 @@ public class JpaLambokApplication {
 //		
 //		operation.deleteById(13);
 //		
-		DeleteOffice empdelete = container.getBean(DeleteOffice.class);
+//		DeleteOffice empdelete = container.getBean(DeleteOffice.class);
 //		
 //		int x = empdelete.deleteByVillage("kolhapur");
 //		System.out.println(x);
 //		
-		int y = empdelete.deleteByVillageAndGender("kolhapur", 'M');
-		System.out.println(y);
+//		int y = empdelete.deleteByVillageAndGender("kolhapur", 'M');
+//		System.out.println(y);
+//		
+//		int z = empdelete.deleteByCityOrGender("kolhapur", "java developer");
 		
-		int z = empdelete.deleteByCityOrGender("kolhapur", "java developer");
+		
 		
 		
 		
